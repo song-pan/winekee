2a64e38da60124ec35814ef555a65281c0655464
song-pan <a909410530@gmail.com>
2020-08-13 20:42:52 +0800

    小购物车因为定位问题搞了几个小时，裂开

--- winekee.sql
+++ winekee.sql
@@ -19,29 +19,28 @@ CREATE TABLE shop_car (
   pname VARCHAR(36) NOT NULL,
   images VARCHAR(200),
   price DECIMAL(7,2),
-  total DECIMAL(7,2),
   pnum INT,
   c_uid INT,
   FOREIGN KEY(c_uid) REFERENCES w_user(uid)
 );
 INSERT INTO shop VALUES(null, '葡萄酒1号', 'images/0001.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒2号', 'images/0002.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒3号', 'images/0003.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒4号', 'images/0004.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒5号', 'images/0005.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒6号', 'images/0006.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒7号', 'images/0007.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒8号', 'images/0022.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒9号', 'images/0009.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒10号', 'images/0010.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒11号', 'images/0011.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒12号', 'images/0012.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒13号', 'images/0013.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒14号', 'images/0014.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒15号', 'images/0015.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒16号', 'images/0016.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒17号', 'images/0018.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒19号', 'images/0019.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒20号', 'images/0020.jpg', "100");
-INSERT INTO shop VALUES(null, '葡萄酒21号', 'images/0021.jpg', "100");
+INSERT INTO shop VALUES(null, '葡萄酒2号', 'images/0002.jpg', "200");
+INSERT INTO shop VALUES(null, '葡萄酒3号', 'images/0003.jpg', "300");
+INSERT INTO shop VALUES(null, '葡萄酒4号', 'images/0004.jpg', "300");
+INSERT INTO shop VALUES(null, '葡萄酒5号', 'images/0005.jpg', "400");
+INSERT INTO shop VALUES(null, '葡萄酒6号', 'images/0006.jpg', "500");
+INSERT INTO shop VALUES(null, '葡萄酒7号', 'images/0007.jpg', "600");
+INSERT INTO shop VALUES(null, '葡萄酒8号', 'images/0022.jpg', "700");
+INSERT INTO shop VALUES(null, '葡萄酒9号', 'images/0009.jpg', "800");
+INSERT INTO shop VALUES(null, '葡萄酒10号', 'images/0010.jpg', "200");
+INSERT INTO shop VALUES(null, '葡萄酒11号', 'images/0011.jpg', "400");
+INSERT INTO shop VALUES(null, '葡萄酒12号', 'images/0012.jpg', "500");
+INSERT INTO shop VALUES(null, '葡萄酒13号', 'images/0013.jpg', "700");
+INSERT INTO shop VALUES(null, '葡萄酒14号', 'images/0014.jpg', "800");
+INSERT INTO shop VALUES(null, '葡萄酒15号', 'images/0015.jpg', "400");
+INSERT INTO shop VALUES(null, '葡萄酒16号', 'images/0016.jpg', "200");
+INSERT INTO shop VALUES(null, '葡萄酒17号', 'images/0018.jpg', "600");
+INSERT INTO shop VALUES(null, '葡萄酒19号', 'images/0019.jpg', "900");
+INSERT INTO shop VALUES(null, '葡萄酒20号', 'images/0020.jpg', "1100");
+INSERT INTO shop VALUES(null, '葡萄酒21号', 'images/0021.jpg', "1800");
 INSERT INTO w_user VALUES(null, 'songpan', 123456, "909410530@qq.com");
